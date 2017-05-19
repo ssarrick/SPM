e6ed7f3ac22d2b5b02ee3ff13bbbb202239d2770
ssarrick <ssarrick@cisco.com>
2017-05-19 13:24:52 -0400

    First commit

---
+++ imapex101_dockerfile/hello_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while [ 0 -eq 0 ]; do
+echo 'Hello World'
+sleep 2
+done
+while [ 0 -eq 0 ]; do
+echo 'Hello World'
+sleep 2
+done
---
+++ imapex101venlab/imapex101_testing_example/doubler.py
@@ -0,0 +1,8 @@
+def doubler(n):
+    """
+    A simple doubler function
+    :param n: int
+    :return: int
+    """
+
+    return 2 * n
